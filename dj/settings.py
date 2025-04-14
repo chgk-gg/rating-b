3ac42a13b176a56b2bc108015465afd21d8bbbbd
Jury Razumau <mail@razumau.net>
2025-04-14 22:14:32 +0200

    add timestamps to logs

--- dj/settings.py
+++ dj/settings.py
@@ -138,9 +138,16 @@ DEBUG = False
 LOGGING = {
     "version": 1,
     "disable_existing_loggers": False,
+    "formatters": {
+        "timestamped": {
+            "format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S"
+        },
+    },
     "handlers": {
         "console": {
             "class": "logging.StreamHandler",
+            "formatter": "timestamped",
         },
     },
     "root": {
