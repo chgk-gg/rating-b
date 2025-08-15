c99a102e41d7c03d3de3e35f0e1172243a95c2c9
Jury Razumau <mail@razumau.net>
2025-08-15 14:31:36 +0200

    switch to uv for packaging

--- cron_scripts/2023.sh
+++ cron_scripts/2023.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Calculates all 2023 releases
-/app/.venv/bin/python /app/manage.py calc_all_releases --first_to_calc 2023-01-05 --last_to_calc 2023-12-28
+/app/manage.py calc_all_releases --first_to_calc 2023-01-05 --last_to_calc 2023-12-28
 
 /app/cron_scripts/refresh_views.sh
--- cron_scripts/2024.sh
+++ cron_scripts/2024.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 # Calculates all 2024 releases
-/app/.venv/bin/python /app/manage.py calc_all_releases --first_to_calc 2024-01-04 --last_to_calc 2024-12-26
+/app/manage.py calc_all_releases --first_to_calc 2024-01-04 --last_to_calc 2024-12-26
 
 /app/cron_scripts/refresh_views.sh
