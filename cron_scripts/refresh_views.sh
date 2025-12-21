5ddf9109e7e9555c2e346244dc17f71646de3be6
Jury Razumau <mail@razumau.net>
2025-12-21 16:00:33 +0100

    preload players and tournaments pages after recalculation

--- cron_scripts/refresh_views.sh
+++ cron_scripts/refresh_views.sh
@@ -2,3 +2,5 @@
 curl "https://rating.chgk.gg/recreate_views/b?token=${RATING_UI_TOKEN}"
 curl "https://rating.chgk.gg/recalculate_truedl/b?token=${RATING_UI_TOKEN}"
 curl "https://rating.chgk.gg/reset_cache?token=${RATING_UI_TOKEN}"
+curl "https://rating.chgk.gg/b/players"
+curl "https://rating.chgk.gg/b/tournaments"
