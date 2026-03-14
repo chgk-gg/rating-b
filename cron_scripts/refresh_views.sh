47959a369c37e60a7f58a7fcee8e292006ae2bc5
Jury Razumau <mail@razumau.net>
2026-03-14 20:50:21 +0100

    hide curl output after runs

--- cron_scripts/refresh_views.sh
+++ cron_scripts/refresh_views.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-curl "https://rating.chgk.gg/recreate_views/b?token=${RATING_UI_TOKEN}"
-curl "https://rating.chgk.gg/recalculate_truedl/b?token=${RATING_UI_TOKEN}"
-curl "https://rating.chgk.gg/reset_cache?token=${RATING_UI_TOKEN}"
-curl "https://rating.chgk.gg/b/players"
-curl "https://rating.chgk.gg/b/tournaments"
+curl "https://rating.chgk.gg/recreate_views/b?token=${RATING_UI_TOKEN}" > /dev/null 2>&1
+curl "https://rating.chgk.gg/recalculate_truedl/b?token=${RATING_UI_TOKEN}" > /dev/null 2>&1
+curl "https://rating.chgk.gg/reset_cache?token=${RATING_UI_TOKEN}" > /dev/null 2>&1
+curl "https://rating.chgk.gg/b/players" > /dev/null 2>&1
+curl "https://rating.chgk.gg/b/tournaments" > /dev/null 2>&1
